7af964da1fdb6fa9f496518e067303fa25464932
Iver Egge <iverasp@gmail.com>
2016-03-28 20:23:17 +0200

    rtfm...

--- ex03/source/flatland_gui.py
+++ ex03/source/flatland_gui.py
@@ -2,19 +2,20 @@ import pygame
 from sys import exit
 from pygame.locals import *
 from os.path import join
+import numpy as np
+
 
 class FlatlandGUI:
 
     # Delay between screen updates (ms)
-    DELAY = 400
+    DELAY = 1500
 
     # Set resources and map to cell integers
     TILESIZE = 80
     empty_tile = pygame.image.load(join('res', 'empty' + str(TILESIZE) + '.png'))
     food_tile = pygame.image.load(join('res', 'food' + str(TILESIZE) + '.png'))
     poison_tile = pygame.image.load(join('res', 'poison' + str(TILESIZE) + '.png'))
-    agent_tile_org = pygame.image.load(join('res', 'agent' + str(TILESIZE) + '.png'))
-    agent_tile = agent_tile_org
+    agent_tile = pygame.image.load(join('res', 'agent' + str(TILESIZE) + '.png'))
 
     tilemap = {
         0: empty_tile,
@@ -22,11 +23,21 @@ class FlatlandGUI:
         2: poison_tile
     }
 
+    # Keep track of our orientation and angle
+    agent_angle = 0
+
+    movemap = {
+        1: (1, 0),
+        2: (0, -1),
+        3: (-1, 0),
+        0: (0, 1)
+    }
+
     """
     Initialize GUI
     Cells is a 2D array containing integers [0, 3)
     denoting the content of a tile: 0 = blank, 1 = food, 2 = posion.
-    Moves is an array of moves where 0 = up, 1 = right, 2 = down, 3 = left.
+    Moves is an array of moves where 0 = forward, 1 = right, 2 = left.
     Start is a tuple of the starting cordinates for the agent.
     """
     def __init__(self, cells, start, moves):
@@ -71,6 +82,11 @@ class FlatlandGUI:
                     print("Delay set to:", self.DELAY)
 
     def update(self):
+
+        # No more moves? Wait until user quits
+        if self.move == len(self.moves):
+            return
+
         # Draw the map
         self.display.fill(Color('white'))
         for row in range(self.mapsize):
@@ -80,6 +96,39 @@ class FlatlandGUI:
                     pygame.rect.Rect(column*self.TILESIZE, row*self.TILESIZE, self.TILESIZE, self.TILESIZE)
                 )
 
+        # Make angles wrap around 360 degrees
+        def update_angle(angle):
+            if self.agent_angle + angle > 360:
+                self.agent_angle = self.agent_angle + angle - 360
+            elif self.agent_angle + angle < 0:
+                self.agent_angle = self.agent_angle + angle + 360
+            else: self.agent_angle = self.agent_angle + angle
+
+        # Forward
+        if self.moves[self.move] == 0:
+            direction = int((self.agent_angle / 360) * 4)
+            if direction == 4: direction = 0
+            self.agent_pos = tuple(
+                np.subtract(self.agent_pos, self.movemap[direction])
+            )
+
+            # Wrap-around world
+            if self.agent_pos[0] < 0: self.agent_pos = (self.mapsize - 1, self.agent_pos[1])
+            elif self.agent_pos[0] > self.mapsize - 1: self.agent_pos = (0, self.agent_pos[1])
+            elif self.agent_pos[1] < 0: self.agent_pos = (self.agent_pos[0], self.mapsize - 1)
+            elif self.agent_pos[1] > self.mapsize - 1: self.agent_pos = (self.agent_pos[0], 0)
+
+
+        # Right
+        elif self.moves[self.move] == 1:
+            update_angle(-90)
+            self.agent_tile = pygame.transform.rotate(self.agent_tile, -90)
+
+        # Left
+        elif self.moves[self.move] == 2:
+            update_angle(+90)
+            self.agent_tile = pygame.transform.rotate(self.agent_tile, +90)
+
         # Draw the agent
         self.display.blit(
             self.agent_tile,
@@ -88,39 +137,11 @@ class FlatlandGUI:
 
         pygame.display.flip()
 
-        # No more moves? Wait until user quits
-        if self.move == len(self.moves) - 1:
-            return
-
         # Check if agent ate something
         self.remove_item(self.agent_pos)
 
-        # Find next position for agent and rotate. Also wrap-around our world
+        # Find next position for agent and rotate
         self.move += 1
-        # Up
-        if (self.moves[self.move]) == 0:
-            self.agent_pos = (self.agent_pos[0], self.agent_pos[1] - 1)
-            if self.agent_pos[1] < 0:
-                self.agent_pos = (self.agent_pos[0], self.mapsize - 1)
-            self.agent_tile = self.agent_tile_org
-        # Right
-        elif (self.moves[self.move]) == 1:
-            self.agent_pos = (self.agent_pos[0] + 1, self.agent_pos[1])
-            if self.agent_pos[0] > self.mapsize - 1:
-                self.agent_pos = (0, self.agent_pos[1])
-            self.agent_tile = pygame.transform.rotate(self.agent_tile_org, 270)
-        # Down
-        elif (self.moves[self.move]) == 2:
-            self.agent_pos = (self.agent_pos[0], self.agent_pos[1] + 1)
-            if self.agent_pos[1] > self.mapsize - 1:
-                self.agent_pos = (self.agent_pos[0], 0)
-            self.agent_tile = pygame.transform.rotate(self.agent_tile_org, 180)
-        # Left
-        elif (self.moves[self.move]) == 3:
-            self.agent_pos = (self.agent_pos[0] - 1, self.agent_pos[1])
-            if self.agent_pos[0] < 0:
-                self.agent_pos = (self.mapsize - 1, self.agent_pos[1])
-            self.agent_tile = pygame.transform.rotate(self.agent_tile_org, 90)
 
     def remove_item(self, position):
         item = self.cells[position[1]][position[0]]
