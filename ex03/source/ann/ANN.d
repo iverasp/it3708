ee2c03680c94ada08cbcfb0b3815237226ab6792
Iver Egge <iverasp@gmail.com>
2016-03-30 20:36:10 +0200

    No time to commit.. My people need me!

--- ex03/source/ann/ANN.d
+++ ex03/source/ann/ANN.d
@@ -21,21 +21,16 @@ class ANN {
 
     public void setWeightsSynapsis0(double[][] weights) {
         this.synapsis0 = new Matrix(weights);
-        writeln(to!string(synapsis0));
     }
 
     public void setWeightsSynapsis1(double[][] weights) {
         this.synapsis1 = new Matrix(weights);
-        writeln(to!string(synapsis1));
     }
 
     public double[][] predict(double[][] input) {
         auto layer0 = new Matrix(input);
-        writeln(to!string(layer0));
         auto layer1 = (layer0 * synapsis0).nonLinear();
-        writeln(to!string(layer1));
         auto layer2 = layer1 * synapsis1;
-        writeln(to!string(layer2));
         return layer2.nonLinear().toArray();
     }
 }
