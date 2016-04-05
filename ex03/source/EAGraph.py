196d7055fc28195349816a9c95b534bc9f4f3d99
Iver Egge <iverasp@gmail.com>
2016-04-05 21:43:46 +0200

    legends

--- ex03/source/EAGraph.py
+++ ex03/source/EAGraph.py
@@ -1,6 +1,7 @@
 from kivy.garden.graph import Graph, MeshLinePlot
 from kivy.uix.boxlayout import BoxLayout
-from math import sin
+from kivy.uix.label import Label
+
 
 class EAGraph(BoxLayout):
     def __init__(self):
@@ -11,7 +12,8 @@ class EAGraph(BoxLayout):
             ylabel="Y",
             x_ticks_minor=5,
             x_ticks_major=25,
-            y_ticks_major=1,
+            y_ticks_minor=5,
+            y_ticks_major=40,
             y_grid_label=True,
             x_grid_label=True,
             padding=5,
@@ -20,14 +22,28 @@ class EAGraph(BoxLayout):
             xmin=0,
             xmax=60,
             ymin=0,
-            ymax=40
+            ymax=40,
+            size=(800, 600)
         )
+
+        red = [1, 0, 0, 1]
+        green = [0, 1, 0, 1]
+        blue = [0, 0, 1, 1]
+
+        self.cols = 2
+        self.orientation = 'vertical'
         self.add_widget(self.graph)
+        legends = BoxLayout()
+        legends.orientation = 'horizontal'
+        legends.add_widget(Label(text='Standard deviation', color=red, size=(100, 200)))
+        legends.add_widget(Label(text='Average fitness', color=green, size=(100, 200)))
+        legends.add_widget(Label(text='Highest fitness', color=blue, size=(100, 200)))
+        self.add_widget(legends)
 
         self.plot = []
-        self.plot.append(MeshLinePlot(color=[1, 0, 0, 1])) #X - Red
-        self.plot.append(MeshLinePlot(color=[0, 1, 0, 1])) #Y - Green
-        self.plot.append(MeshLinePlot(color=[0, 0, 1, 1])) #Z - Blue
+        self.plot.append(MeshLinePlot(color=red)) #X - Red
+        self.plot.append(MeshLinePlot(color=green)) #Y - Green
+        self.plot.append(MeshLinePlot(color=blue)) #Z - Blue
 
         for plot in self.plot:
             self.graph.add_plot(plot)
