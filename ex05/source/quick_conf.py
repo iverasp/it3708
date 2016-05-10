fc974c868001f2157f6ec8a8abea9c91e7ddf9af
johannsl <johannsl@stud.ntnu.no>
2016-05-10 21:55:30 +0200

    conf

--- ex05/source/quick_conf.py
+++ ex05/source/quick_conf.py
@@ -3,8 +3,8 @@ class QuickConf:
     population_size = 1000
     number_of_children = 1000
     genotype_length = 48
-    tournament_epsilon = 0.9
-    tournament_group_size = 10
-    crossover_rate = 0.9
+    tournament_epsilon = 0.1
+    tournament_group_size = 100
+    crossover_rate = 1.0
     children_per_pair = 2
-    mutation_rate = 0.9
+    mutation_rate = 1.0
